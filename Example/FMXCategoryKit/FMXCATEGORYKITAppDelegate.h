1765e30eac6568877a92f7345cc7fef1b0138ee6
wnsj <>
2020-09-16 17:05:22 +0800

    Initial commit

---
+++ Example/FMXCategoryKit/FMXCATEGORYKITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMXCATEGORYKITAppDelegate.h
+//  FMXCategoryKit
+//
+//  Created by 1351893134@qq.com on 09/16/2020.
+//  Copyright (c) 2020 1351893134@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FMXCATEGORYKITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/FMXCategoryKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FMXCategoryKit
+//
+//  Created by 1351893134@qq.com on 09/16/2020.
+//  Copyright (c) 2020 1351893134@qq.com. All rights reserved.
+//
+
+@import UIKit;
+#import "FMXCATEGORYKITAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FMXCATEGORYKITAppDelegate class]));
+    }
+}
